f7b541738e391937906c936d01334ccb616d2c22
Ahmad Fahrizal Ramadhan <97895281+ahmadfahrizalr@users.noreply.github.com>
2022-01-18 11:04:38 +0800

    Perkalian dan Pembagian

---
+++ perkalian-pembagian.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+int main()
+{
+    int a = 2, b = 5, c = 4;
+    int d;
+
+    d = a*b/(c+b);
+
+    cout << "Maka nilai d = " << d << '\n';
+    return 0;
+}
