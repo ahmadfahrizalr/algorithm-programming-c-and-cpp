ebea23b3cf2dd90a6310ba2dcb6a8a1c5563e94c
Ahmad Fahrizal Ramadhan <97895281+ahmadfahrizalr@users.noreply.github.com>
2022-01-18 11:13:43 +0800

    Program Void

---
+++ program-void.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+void tampil (int bil)
+{
+    cout << "anda menampilkan bilangan "<< bil << endl;
+}
+int main ()
+{
+    cout << "menampilkan bilangan lewat sub-fungsi\n";
+    tampil (8);
+
+    return 0;
+}
