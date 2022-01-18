cc5cecb316580f90ea05b623c2cec8d66d04fce5
Ahmad Fahrizal Ramadhan <97895281+ahmadfahrizalr@users.noreply.github.com>
2022-01-18 11:02:18 +0800

    Perkalian Dua

---
+++ perkalian-dua.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+int main()
+{
+    int nilai, kali_dua;
+
+    cout << "Masukkan nilai mahasiswa : ";
+    cin >> nilai;
+
+    kali_dua = nilai * 2;
+    cout << "Perkalian dua terhadap nilai adalah : " << kali_dua << endl;
+
+    return 0;
+}
