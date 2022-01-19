200d02321614c853b596589ea60d07aff3a2b9c5
Ahmad Fahrizal Ramadhan <97895281+ahmadfahrizalr@users.noreply.github.com>
2022-01-19 15:07:18 +0800

    Program Biodata Mahasiswa

---
+++ program-biodata-mahasiswa.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <stdio.h>
+using namespace std;
+int main()
+{
+    char nama[50],nim[10];
+    cout<<"Masukan Nama = ";
+    gets(nama);
+    cout<<"Masukan NIM = ";
+    gets(nim);
+    cout<<endl<<endl;
+    cout<<"BIODATA MAHASISWA";
+    cout<<endl;
+    cout<<"Nama : "<<nama;
+    cout<<endl;
+    cout<<"NIM : "<<nim;
+    cout<<endl;
+    return 0;
+}
