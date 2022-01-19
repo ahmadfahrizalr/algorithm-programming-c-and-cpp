acb832f3d1df6930f3610390ddbadf1d0d0a3ba5
Ahmad Fahrizal Ramadhan <97895281+ahmadfahrizalr@users.noreply.github.com>
2022-01-19 14:54:52 +0800

    Hello World

---
+++ hello-world.cpp
@@ -0,0 +1,9 @@
+//PROGRAM MENAMPILKAN HELLO WORD
+
+#include <iostream> //Menentukan file header
+using namespace std;
+int main()
+{
+    cout << "Welcome to C++!\n"; //Menampilkan perintah program
+    return 0; //Kembali keadaan semula
+}
