583b617406402916fbab1918131f8dc7ed4a5206
Ahmad Fahrizal Ramadhan <97895281+ahmadfahrizalr@users.noreply.github.com>
2022-01-19 17:44:21 +0800

    Masukkan Bilangan

---
+++ masukkan-bilangan.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+typedef int Bilangan;
+int main()
+{
+    Bilangan B;
+    cout << "Masukkan satu bilangan: ";
+    cin >> B;
+    cout << endl << "Bilangan yang ada masukkan adalah " <<
+    B << endl;
+    return 0;
+}
